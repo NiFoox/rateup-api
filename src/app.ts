7d80a9ab39f935e076d911bc6035e007a4f4f851
NiFoox <nifoox@gmail.com>
2025-07-25 18:58:24 -0300

    feat: endpoint de prueba para validar Express

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import express from 'express';
 
 const app = express();
+const PORT = 3000;
+
+app.get('/api/status', (req, res) => {
+  res.json({ status: 'Prueba funcionando' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
